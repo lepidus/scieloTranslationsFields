a84a88a518bbea03837413d6614bf21a5660791f
Jhon <jhon@lepidus.com.br>
2024-12-04 15:00:06 -0400

    Adds DOI validator class
    Issue: documentacao-e-tarefas/scielo#724
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/DoiValidator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace APP\plugins\generic\scieloTranslationsFields\classes;
+
+class DoiValidator
+{
+    public function validate(string $doi): bool
+    {
+        return preg_match('/^10\.\d{4,9}\/[-._;()\/:A-Z0-9]+$/i', $doi);
+    }
+}
