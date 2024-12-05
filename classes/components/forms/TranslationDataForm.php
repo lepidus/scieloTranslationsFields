4b152120d297c3613592a025066c2e520e85796b
Jhon <jhon@lepidus.com.br>
2024-12-05 13:13:21 -0400

    Removes field from translation data form and updates locales
    Issue: documentacao-e-tarefas/scielo#725
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/components/forms/TranslationDataForm.php
+++ classes/components/forms/TranslationDataForm.php
@@ -18,29 +18,12 @@ class TranslationDataForm extends FormComponent
         $originalDocumentDoi = $publication->getData('originalDocumentDoi');
 
         $this->action = $action . "&placedOn=$placedOn";
-        $this->addField(new FieldOptions('originalDocumentHasDoi', [
-            'label' => __('plugins.generic.scieloTranslationsFields.originalDocumentHasDoi'),
-            'description' => __('plugins.generic.scieloTranslationsFields.originalDocumentHasDoi.description'),
-            'type' => 'radio',
-            'isRequired' => true,
-            'value' => $publication->getData('originalDocumentHasDoi'),
-            'options' => [
-                [
-                    'value' => 1,
-                    'label' => __('plugins.generic.scieloTranslationsFields.originalDocumentHasDoi.yes')
-                ],
-                [
-                    'value' => 0,
-                    'label' => __('plugins.generic.scieloTranslationsFields.originalDocumentHasDoi.no')
-                ]
-            ],
-        ]))
-        ->addField(new FieldText('originalDocumentDoi', [
+        $this->addField(new FieldText('originalDocumentDoi', [
             'label' => __('plugins.generic.scieloTranslationsFields.originalDocumentDoi'),
             'description' => __('plugins.generic.scieloTranslationsFields.originalDocumentDoi.description'),
             'isMultilingual' => false,
+            'isRequired' => true,
             'value' => $originalDocumentDoi,
-            'showWhen' => ['originalDocumentHasDoi', 1]
         ]));
 
         if (!empty($originalDocumentDoi) && $placedOn == 'workflow') {
@@ -53,7 +36,6 @@ class TranslationDataForm extends FormComponent
             $this->addField(new FieldHTML('originalDocumentDoi', [
                 'label' => __('plugins.generic.scieloTranslationsFields.originalDocumentCitation'),
                 'description' => "<p style=\"text-align: justify\">{$originalDocumentCitation}</p>",
-                'showWhen' => ['originalDocumentHasDoi', 1]
             ]));
         }
     }
