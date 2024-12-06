0babef3825f69093346991e8ece2912880eadd80
Jhon <jhon@lepidus.com.br>
2024-12-06 17:08:49 -0400

    Gets translator user group in validator class
    Issue: documentacao-e-tarefas/scielo#726
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/FieldsValidator.php
+++ classes/FieldsValidator.php
@@ -2,10 +2,30 @@
 
 namespace APP\plugins\generic\scieloTranslationsFields\classes;
 
+use PKP\userGroup\UserGroup;
+use APP\facades\Repo;
+
 class FieldsValidator
 {
     public function validateDoi(string $doi): bool
     {
         return (preg_match('/^10\.\d{4,9}\/[-._;()\/:A-Z0-9]+$/i', $doi) === 1);
+    }
+
+    public function getTranslatorsUserGroup(int $contextId): ?UserGroup
+    {
+        $contextUserGroups = Repo::userGroup()->getCollector()
+            ->filterByContextIds([$contextId])
+            ->getMany();
+
+        foreach ($contextUserGroups as $userGroup) {
+            $userGroupAbbrev = strtolower($userGroup->getData('abbrev', 'en'));
+
+            if ($userGroupAbbrev === 'tr') {
+                return $userGroup;
+            }
+        }
+
+        return null;
     }
 }
--- tests/FieldsValidatorTest.php
+++ tests/FieldsValidatorTest.php
@@ -1,10 +1,49 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+use PKP\tests\DatabaseTestCase;
+use PKP\userGroup\UserGroup;
+use PKP\security\Role;
+use APP\facades\Repo;
 use APP\plugins\generic\scieloTranslationsFields\classes\FieldsValidator;
 
-class FieldsValidatorTest extends TestCase
+class FieldsValidatorTest extends DatabaseTestCase
 {
+    private $translatorsUserGroup;
+    private $contextId = 1;
+    private $locale = 'en';
+
+    public function setUp(): void
+    {
+        $this->translatorsUserGroup = $this->createTranslatorsUserGroup();
+    }
+
+    protected function tearDown(): void
+    {
+        Repo::userGroup()->delete($this->translatorsUserGroup);
+        parent::tearDown();
+    }
+
+    private function createTranslatorsUserGroup()
+    {
+        $translatorsUserGroup = new UserGroup();
+        $translatorsUserGroup->setAllData([
+            'contextId' => $this->contextId,
+            'roleId' => Role::ROLE_ID_AUTHOR,
+            'isDefault' => true,
+            'showTitle' => false,
+            'permitSelfRegistration' => false,
+            'permitMetadataEdit' => true,
+            'abbrev' => [
+                $this->locale => 'TR'
+            ]
+        ]);
+
+        $translatorsUserGroupId = Repo::userGroup()->add($translatorsUserGroup);
+        $translatorsUserGroup->setId($translatorsUserGroupId);
+
+        return $translatorsUserGroup;
+    }
+
     public function testValidateValidDois()
     {
         $fieldsValidator = new FieldsValidator();
@@ -21,5 +60,13 @@ class FieldsValidatorTest extends TestCase
         $this->assertFalse($fieldsValidator->validateDoi('10.1234'));
         $this->assertFalse($fieldsValidator->validateDoi('10.1038/abc!123'));
         $this->assertFalse($fieldsValidator->validateDoi('10./1000/abcd'));
+    }
+
+    public function testGetTranslatorsUserGroup()
+    {
+        $fieldsValidator = new FieldsValidator();
+        $retrievedUserGroup = $fieldsValidator->getTranslatorsUserGroup($this->contextId);
+
+        $this->assertEquals($this->translatorsUserGroup->getId(), $retrievedUserGroup->getId());
     }
 }
