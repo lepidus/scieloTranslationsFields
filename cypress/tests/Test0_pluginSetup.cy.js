25bdd80f72569c7bc804219486f144403efabda2
Jhon <jhon@lepidus.com.br>
2024-12-11 12:45:31 -0400

    Integration tests - Adds ORCID to user profile
    Issue: documentacao-e-tarefas/scielo#726
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -9,4 +9,13 @@ describe('Plugin configuration', function () {
 		cy.get('input[id^=select-cell-scielotranslationsfieldsplugin]').check();
 		cy.get('input[id^=select-cell-scielotranslationsfieldsplugin]').should('be.checked');
 	});
+	it('Adds ORCID to user profile', function () {
+		cy.login('ckwantes', null, 'publicknowledge');
+		cy.get('.app__headerActions button').eq(1).click();
+        cy.contains('a', 'Edit Profile').click();
+
+		cy.get('a[name="publicProfile"]').click();
+		cy.get('input[name="orcid"]').type('https://orcid.org/0000-0002-1825-0097', {delay: 0});
+		cy.get('.submitFormButton:visible').click();
+	});
 });
